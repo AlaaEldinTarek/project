1092551e3d5d93b201b34213a60f07948f9551e7
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-01-26 06:15:52 +0200

    up

--- src/db.js
+++ src/db.js
@@ -1,17 +1,95 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getDatabase,
+  ref,
+  set,
+  update,
+} from "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js";
 
-// Your web app's Firebase configuration
+// web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA9TpzPneOPOEkdGhcpX39m239Gw58vdsQ",
   authDomain: "users-login-4c009.firebaseapp.com",
   projectId: "users-login-4c009",
   storageBucket: "users-login-4c009.appspot.com",
   messagingSenderId: "765968837432",
-  appId: "1:765968837432:web:c9cc70a24068217451afcd"
+  appId: "1:765968837432:web:c9cc70a24068217451afcd",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getDatabase(app);
+
+const createAccountForm = document.getElementById("submitData");
+const login = document.getElementById("submit");
+
+login.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  var password = document.getElementById("loginPassword").value;
+  var email = document.getElementById("username").value;
+
+  signInWithEmailAndPassword(auth,email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+      var lgDate = new Date();
+      update(ref(db, "users/" + user.uid), {
+        last_login: lgDate,
+      })
+        .then(() => {
+          alert("Login successfully ");
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
+});
+
+createAccountForm.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  var email = document.getElementById("emailAddress").value;
+  var password = document.getElementById("password").value;
+  var username = document.getElementById("signupUsername").value;
+
+  createUserWithEmailAndPassword(auth, email, password, username)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+      set(ref(db, "users/" + user.uid), {
+        username: username,
+        password: password,
+        email: email,
+      })
+        .then(() => {
+          alert(
+            "Your account has been created successfully with " + username + "."
+          );
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+      alert(errorCode + ": " + errorMessage);
+    });
+});
--- src/main.js
+++ src/main.js
@@ -1,106 +1,54 @@
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA9TpzPneOPOEkdGhcpX39m239Gw58vdsQ",
-  authDomain: "users-login-4c009.firebaseapp.com",
-  projectId: "users-login-4c009",
-  storageBucket: "users-login-4c009.appspot.com",
-  messagingSenderId: "765968837432",
-  appId: "1:765968837432:web:c9cc70a24068217451afcd",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
 function setFormMessage(formElement, type, message) {
   const messageElement = formElement.querySelector(".form__message");
 
   messageElement.textContent = message;
-  messageElement.classList.remove(
-    "form__message--success",
-    "form__message--error"
-  );
+  messageElement.classList.remove("form__message--success", "form__message--error");
   messageElement.classList.add(`form__message--${type}`);
 }
 
 function setInputError(inputElement, message) {
   inputElement.classList.add("form__input--error");
-  inputElement.parentElement.querySelector(
-    ".form__input-error-message"
-  ).textContent = message;
+  inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
 }
 
 function clearInputError(inputElement) {
   inputElement.classList.remove("form__input--error");
-  inputElement.parentElement.querySelector(
-    ".form__input-error-message"
-  ).textContent = "";
+  inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
   const createAccountForm = document.querySelector("#createAccount");
 
-  document
-    .querySelector("#linkCreateAccount")
-    .addEventListener("click", (e) => {
+  document.querySelector("#linkCreateAccount").addEventListener("click", e => {
       e.preventDefault();
       loginForm.classList.add("form--hidden");
       createAccountForm.classList.remove("form--hidden");
-    });
-
-  document.querySelector("#linkLogin").addEventListener("click", (e) => {
-    e.preventDefault();
-    loginForm.classList.remove("form--hidden");
-    createAccountForm.classList.add("form--hidden");
   });
 
-  loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    var email = document.getElementById('emailAddress').value;
-    var password = document.getElementById('password').value;
-    // Perform your AJAX/Fetch login
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        // ...
-        alert("Your account has been created successfully with " + user + ".");
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-        alert(errorCode + ": " + errorMessage);
+  document.querySelector("#linkLogin").addEventListener("click", e => {
+      e.preventDefault();
+      loginForm.classList.remove("form--hidden");
+      createAccountForm.classList.add("form--hidden");
+  });
+
+  loginForm.addEventListener("submit", e => {
+      e.preventDefault();
+
+      // Perform your AJAX/Fetch login
+
+      setFormMessage(loginForm, "error", "Invalid username/password combination");
+  });
+
+  document.querySelectorAll(".form__input").forEach(inputElement => {
+      inputElement.addEventListener("blur", e => {
+          if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+              setInputError(inputElement, "Username must be at least 10 characters in length");
+          }
       });
-    setFormMessage(loginForm, "error", "Invalid username/password combination");
-  });
 
-  document.querySelectorAll(".form__input").forEach((inputElement) => {
-    inputElement.addEventListener("blur", (e) => {
-      if (
-        e.target.id === "signupUsername" &&
-        e.target.value.length > 0 &&
-        e.target.value.length < 10
-      ) {
-        setInputError(
-          inputElement,
-          "Username must be at least 10 characters in length"
-        );
-      }
-    });
-
-    inputElement.addEventListener("input", (e) => {
-      clearInputError(inputElement);
-    });
+      inputElement.addEventListener("input", e => {
+          clearInputError(inputElement);
+      });
   });
 });
